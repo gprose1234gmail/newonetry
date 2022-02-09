491259c6b6c1d2c86ef2d734cd664751e024ca69
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-02-09 10:33:12 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -79,7 +79,7 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-yurikorobot_IMG = "https://telegra.ph/file/8b6f8f2bb4ff3912634c7.jpg"
+yurikorobot_IMG = "https://telegra.ph/file/c329e8bb9b2157b963fb9.jpg"
 
 PM_START_TEXT = """
 *👋 Hello {} !*
@@ -90,7 +90,8 @@ PM_START_TEXT = """
 × *Uᴘᴛɪᴍᴇ:* `{}`
 × `{}` *Uꜱᴇʀ, Aᴄʀᴏꜱꜱ* `{}` *Cʜᴀᴛꜱ.*
 ────────────────────────
-✗ *Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: TEᗩᗰ ᒪᑌᑎᗩ!*
+ƒєєℓ тнє ℓσνє🦋💘
+✗ *Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: [TEᗩᗰ ᒪᑌᑎᗩ](https://telegra.ph/file/c329e8bb9b2157b963fb9.jpg)!*
 """
 
 buttons = [
